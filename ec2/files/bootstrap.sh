bc79decc3dce10cb544b3cc642c857969787222c
Alex Mendes <alexmendes.unix@gmail.com>
2024-04-02 13:28:41 +0100

    Fix /home Typo in bootstrap.sh

--- ec2/files/bootstrap.sh
+++ ec2/files/bootstrap.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Required Variables (Change these values according to your needs)
-USERNAME="alexmbarbosa"
+# Required Variables (Change these values according to your confs)
+USERNAME="sysadmin"
 GROUPNAME="devops"
-PUBLIC_KEY="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK1CjAysvZdq5Ch/as4s8R/wvXvWMwpP1V5VPMSkO5KH"
+PUBLIC_KEY="ssh-ed25519 HASHCODEXXX....."
 
 # Shell Script Functions -------------------------------------------------------------------
 command_exists() {
@@ -11,6 +11,7 @@ command_exists() {
 }
 
 # Shell Script Code
+
 # Create GROUPNAME/USERNAME
 sudo groupadd "${GROUPNAME}"
 echo "%${GROUPNAME} ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/"${GROUPNAME}"
@@ -19,7 +20,7 @@ sudo useradd -m -G "${GROUPNAME}" "${USERNAME}"
 # Configure sysadmin ssh public key in authorized keys
 sudo mkdir -p /home/"${USERNAME}"/.ssh
 echo "${PUBLIC_KEY}" | sudo tee -a /home/"${USERNAME}"/.ssh/authorized_keys
-sudo chown -R "${USERNAME}":"${GROUPNAME}" home/"${USERNAME}"/.ssh
+sudo chown -R "${USERNAME}":"${GROUPNAME}" /home/"${USERNAME}"/.ssh
 sudo chmod 440 /etc/sudoers.d/"${GROUPNAME}"
 sudo chmod 700 /home/"${USERNAME}"/.ssh
 sudo chmod 600 /home/"${USERNAME}"/.ssh/authorized_keys
@@ -34,7 +35,7 @@ for package in "${packages[@]}"; do
 done
 
 if ! command_exists java || [[ "$(java -version 2>&1 | grep 'java version')" == "" ]]; then
-	sudo yum install -y java-21 java-21-devel
+	sudo yum install -y java java-devel
 fi
 
 # install terraform
@@ -46,20 +47,15 @@ sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.29.0/
 sudo chmod +x ./kubectl
 sudo mkdir -p /home/"${USERNAME}"/bin && sudo cp ./kubectl /home/"${USERNAME}"/bin/kubectl && export PATH="${PATH}":/home/"${USERNAME}"/bin
 
-# install jenkins
+# install jenkins on AmazonLinux2
 if [[ ! -f "/etc/yum.repos.d/jenkins.repo" ]]; then
 	sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 	sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 fi
 
-# Update packages
+# Update && install jenkins
 sudo yum upgrade -y
-
-# Jenkins install
-if ! command_exists jenkins; then
-	sudo yum install -y jenkins
-fi
+sudo yum install jenkins -y
 
 # Jenkins Daemon
-sudo systemctl daemon-reload
-sudo systemctl enable --now jenkins.service
+sudo systemctl enable jenkins && sudo systemctl start jenkins
